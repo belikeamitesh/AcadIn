3935de632547d22ce1a0f9720941a7792e422bbc
AC-BOSS <amartya.dps@gmail.com>
2022-03-09 23:27:13 +0530

    changed naming conventions and used module.css

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,20 @@
-import Home from "./pages/home/home"
-import React from "react"
+import React from "react";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 function App() {
-  return <Home />
+  return (
+    // <Login/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
